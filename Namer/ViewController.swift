01bc431d27102e6484c552155b62bc7ce3be459e
Ironbatpug <molnarcsaba92@gmail.com>
2019-11-03 21:19:52 +0100

    first commit

---
+++ Namer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Namer
+//
+//  Created by Molnár Csaba on 2019. 11. 03..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
