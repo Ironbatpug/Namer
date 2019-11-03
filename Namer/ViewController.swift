0cb5a61e299b23f7884a1a77dc5d3a249c0757d4
Ironbatpug <molnarcsaba92@gmail.com>
2019-11-03 22:24:58 +0100

    adding the button and textfield logic

--- Namer/ViewController.swift
+++ Namer/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var helloLbl: UILabel!
+    @IBOutlet weak var nameEntryTextField: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    @IBOutlet weak var namesLbl: UILabel!
+    
+    var disposeBag = DisposeBag()
+    var namesArray: Variable<[String]> = Variable([])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bindtextfield()
+        bindSubmitButton()
     }
-
-
+    
+    func bindtextfield() {
+        nameEntryTextField.rx.text
+            .map {
+                if $0 == "" {
+                    return "TYPE YOUR NAME BELOW."
+                } else {
+                    return "Hello, \($0!)."
+                }
+        }
+        .bind(to: helloLbl.rx.text)
+        .disposed(by: disposeBag)
+    }
+    
+    func bindSubmitButton() {
+        submitBtn.rx.tap
+            .subscribe(onNext: {
+                if self.nameEntryTextField.text != "" {
+                    self.namesArray.value.append(self.nameEntryTextField.text!)
+                    self.namesLbl.rx.text.onNext(self.namesArray.value.joined(separator: ", "))
+                    self.nameEntryTextField.rx.text.onNext("")
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
 }
 
